1fd92eb1abda21e58957db5c6234a9a2e3862598
Iverson-Vargas <iversonvargas8@gmail.com>
2026-02-25 03:25:33 -0400

    probando talwin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ export const Login = () => {
     return (
         <>
             <h1>Login</h1>
-            <p>Esto es el login</p>
+            <p class="text-9xl">Esto es el login</p>
         </>
     )
 }
