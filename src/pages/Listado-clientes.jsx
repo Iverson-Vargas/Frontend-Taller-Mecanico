eb83f80d6fabd9ccacfed6033fd8ae9710047502
Isis Cedeño <isis01sofic@gmail.com>
2026-02-28 17:00:00 -0400

    rutas de listado

---
+++ src/pages/Listado-clientes.jsx
@@ -0,0 +1,11 @@
+
+import { useNavigate } from 'react-router-dom';
+
+export const ListaClientes = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="tabla">
+            <h2>Listado de clientes hh</h2>
+        </div>
+    );
+};
---
+++ src/pages/Listado-servicio.jsx
@@ -0,0 +1,12 @@
+
+import { useNavigate } from 'react-router-dom';
+
+
+export const ListaServicio = () => {
+    const navigate = useNavigate();
+    return (
+
+            <h2>Listado de servicios hh</h2>
+  
+    );
+};
